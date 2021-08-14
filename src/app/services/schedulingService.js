17aa458577755ad8462f1165d950a3bd61db4b81
Mashood Rafi <pro@Mashood-MacBook-Pro-2019.local>
2021-08-14 17:51:08 +0500

    Add comments in schedulingCommands file.

--- src/app/services/schedulingService.js
+++ src/app/services/schedulingService.js
@@ -4,7 +4,7 @@ import cacheService from "./cacheService";
 const schedulingCommands = async () => {
 
     /*
-        This cron command will run every 10 minutes and delete older records.
+        This cron command will run every 10 minutes and delete older records that have exceeded the size of collection limit defined.
     */
     nodeSchedule.scheduleJob('*/10 * * * *', () => {
         cacheService.clearCache();
